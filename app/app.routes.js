953977965f7949f7276cfab8129d256d0ce7a511
TwiiiXeR <36134927+AshrafSamir@users.noreply.github.com>
2022-05-17 08:43:41 +0200

    Clean: 404 page removed

--- app/app.routes.js
+++ app/app.routes.js
@@ -1,10 +1,10 @@
 angular.module('appModule')
   .config(($locationProvider) => {
     $locationProvider.html5Mode({
-      enabled: false,
+      enabled: true,
     });
   })
-  .config(($stateProvider, $urlRouterProvider) => {
+  .config(($stateProvider) => {
     $stateProvider
       .state({
         name: 'app',
@@ -22,15 +22,5 @@ angular.module('appModule')
         name: 'team-performance',
         url: '/team-performance',
         template: '<v-performance-page></v-performance-page>',
-      })
-      .state({
-        name: '404',
-        template: '<v-page-not-found></v-page-not-found>',
       });
-
-    $urlRouterProvider.otherwise(function ($injector, $location) {
-      var state = $injector.get('$state');
-      state.go('404');
-      return $location.path();
-    });
   });
